09586b79c83a99a099428b461d3ae4e7899273f6
nao20010128nao <nao20010128@gmail.com>
2015-07-12 22:03:55 +0900

    いろいろ

--- KanamozicCore/KanamozicTest/UnitTest1.vb
+++ KanamozicCore/KanamozicTest/UnitTest1.vb
@@ -1,11 +1,22 @@
 ﻿Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
-
+Imports Kanamozic
+'The result is from http://nao20010128nao.github.io/kanamozic
 <TestClass()>
 Public Class UnitTest1
 
     <TestMethod()>
-    Public Sub TestMethod1()
+    Public Sub 日本語EncodeTest()
+        If Kanamozic.Kanamozic.Encode("日本語", 0) <> "ぎるタオシるタノテりさチオ" Then
+            Throw New Exception
+        End If
+    End Sub
+
+    <TestMethod()>
+    Public Sub ぎるタオシるタノテりさチオDecodeTest()
+        If Kanamozic.Kanamozic.Decode("ぎるタオシるタノテりさチオ", 0) <> "日本語" Then
+            Throw New Exception
+        End If
     End Sub
 
 End Class
