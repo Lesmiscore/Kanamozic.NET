62724dd8f5c3884682af36f738c3d19de7e9eda8
nao20010128nao <nao20010128@gmail.com>
2015-07-12 20:06:06 +0900

    ほぼ完成

--- KanamozicCore/KanamozicCore/Kanamozic.vb
+++ KanamozicCore/KanamozicCore/Kanamozic.vb
@@ -42,6 +42,43 @@ Public Class Kanamozic
         Return buf
     End Function
 
+    Public Shared Function Decode(s As String, key As SByte) As String
+        If s(0) = "が"c Then
+            Return DecodeKana(s.Substring(1), key)
+        ElseIf s(0) = "ぎ"c Then
+            Return DecodeBinary(s.Substring(1), key)
+        Else
+            Throw New ArgumentException(s + " is incorrect format. The string must start with 'が'(ga) or 'ぎ'(gi).")
+        End If
+    End Function
+    Private Shared Function DecodeKana(s As String, key As SByte) As String
+        Dim buf = New Char(s.Length - 1) {}
+        For i = 0 To s.Length - 1
+            Dim value As Char = s(i)
+            Dim offset = FullJpnLetters.IndexOf(value)
+            Dim requiredOffset = IncreaseIfNegative(offset - key, FullJpnLetters.Length) Mod FullJpnLetters.Length
+            buf(i) = FullJpnLetters(requiredOffset)
+        Next
+        Return buf
+    End Function
+    Private Shared Function DecodeBinary(s As String, key As SByte) As String
+        Dim buf = New Char(s.Length - 1) {}
+        For i = 0 To s.Length - 1
+            Dim value As Char = s(i)
+            Dim offset = ConvertJpnLetters.IndexOf(value)
+            Dim requiredOffset = IncreaseIfNegative(offset - key, ConvertEngLetters.Length) Mod ConvertEngLetters.Length
+            buf(i) = ConvertEngLetters(requiredOffset)
+        Next
+        Return DecodeBase64(buf)
+    End Function
+
+    Private Shared Function IncreaseIfNegative(value As Integer, once As Integer) As Integer
+        While value < 0
+            value += once
+        End While
+        Return value
+    End Function
+
     Private Shared Function ToByteArray(s As String) As Byte()
         Return Encoding.UTF8.GetBytes(s)
     End Function
